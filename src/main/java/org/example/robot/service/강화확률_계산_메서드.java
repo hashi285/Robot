4ff48d7cf8b4b6648be0e0685be1894241eb3ce5
hashi285 <m51187251@gmail.com>
2024-10-02 12:27:24 +0900

    강화 성공 확률 메서드

---
+++ src/main/java/org/example/robot/service/강화확률_계산_메서드.java
@@ -0,0 +1,14 @@
+package org.example.robot.service;
+
+public class 강화확률_계산_메서드 {
+    public static void main(String[] args) {
+        /*
+        - 강화 시도 횟수 ( 1강부터 8강까지 )
+        - 강화 성공 횟수 ( 1강부터 8강까지 )
+        - 강화 실패 횟수 ( 1강부터 8강까지 )
+        - 각각의 (2 ~ 8)강 강화 성공 확률
+        - 8강 성공까지 걸린 시간
+        -
+         */
+    }
+}
